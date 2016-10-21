cb8637bb40e83b27046b6c02161c2758b18883c6
Asheq Reza <Asheq Reza>
2016-10-21 10:36:58 -0400

    modified chat

--- slytherin/app.py
+++ slytherin/app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
+	
+
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -14,15 +16,16 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
 
+
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
-    while True:
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
+    #while True:
+    #    socketio.sleep(10)
+	#count += 1
+	#socketio.emit('my_response',
+	#			  {'data': 'Server generated event', 'count': count},
+	#			  namespace='/test')
 
 
 @app.route('/')
