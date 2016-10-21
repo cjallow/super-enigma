5ad2ee9fef16a90148d4f70c05a6a948aee1ff7d
Edgar Ruiz <ed.ruiz1106@gmail.com>
2016-10-21 11:32:06 -0400

    added

--- slytherin/app.py
+++ slytherin/app.py
@@ -3,8 +3,150 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
+import requests
+import json
+
+import nltk
+from nltk import word_tokenize, pos_tag
+import string
+import re
+
+keywords = [];
+query = ["cik","companyname","entityid","primaryexchange",
+"marketoperator","markettier","primarysymbol","siccode","sicdescription"
+,"usdconversionrate","restated","receiveddate","preliminary","periodlengthcode","periodlength"
+,"periodenddate","original","formtype","fiscalyear","fiscalquarter","dcn","currencycode",
+"crosscalculated","audited","amended","changeincurrentassets","changeincurrentliabilities"
+,"changeininventories","dividendspaid","effectofexchangerateoncash","capitalexpenditures"
+,"cashfromfinancingactivities","cashfrominvestingactivities","cashfromoperatingactivities"
+,"cfdepreciationamortization","changeinaccountsreceivable","investmentchangesnet","netchangeincash"
+,"totaladjustments","ebit","costofrevenue","grossprofit","incomebeforetaxes","netincome"
+,"netincomeapplicabletocommon","researchdevelopmentexpense","totalrevenue","sellinggeneraladministrativeexpenses"
+,"commonstock","cashandcashequivalents","cashcashequivalentsandshortterminvestments","goodwill"
+,"intangibleassets","inventoriesnet","otherassets","othercurrentassets","othercurrentliabilities"
+,"otherliabilities","propertyplantequipmentnet","retainedearnings","totalassets","totalcurrentassets"
+,"totalcurrentliabilities","totalliabilities","totallongtermdebt","totalreceivablesnet","totalshorttermdebt"
+,"totalstockholdersequity"]
+
+quarters = {"first" : 1, "second" : 2, "third" : 3, "fourth":4}
+dict = {}
+
+
+# Get all the English alphabet letters and a space
+valid_letters = string.ascii_letters  + string.digits + ' '
+
+pattern = '[1-3][0-9]{3}'
+
+#match = re.findall(r'(\d+/\d+/\d+)',text)
+
+#stripped_text = ''.join([char if char in valid_letters else '' for char in text])
+#tokens = word_tokenize(stripped_text)
+#tags = pos_tag(tokens)
+
+#for (a,b) in tags:
+#	if b == 'NN' or b == 'NNP':
+#		keywords.append(a)
+#	if b == 'NNP':
+#		dict["companyname"] = a
+#	if a == 'first' or a == 'second' or a == 'third' or a == 'fourth':
+#		dict["financialquarter"] = quarters[a]
+
+#if match:		
+#	for a in match:
+		
+#		dict["date"] = a
+	
+def parsetext(text):
+	stripped_text = ''.join([char if char in valid_letters else '' for char in text])
+	tokens = word_tokenize(stripped_text)
+	tags = pos_tag(tokens)
+	match = re.findall(pattern,text)
+	dict["fieldToSearch"] = []
+	for (a,b) in tags:
+		if b == 'NN' or a =='gross':
+			keywords.append(a)
+		if b == 'NNP':
+			dict["companyname"] = a
+		if a == 'first' or a == 'second' or a == 'third' or a == 'fourth':
+			dict["financialquarter"] = quarters[a]
+			
+	for a in keywords:
+		dict["fieldToSearch"].append(a)
+
+	if match:		
+		for a in match:
+			if "date" in dict:
+				dict["date2"] = a
+			else:
+				dict["date"] = a 
+				
+	print (dict)
+	return dict
+	
+	
+	#if a =='debt':
+	#		dict["fieldToSearch"] = a
+	#	elif a =='growth':
+	#		dict["fieldToSearch"] = a
+	#	elif a =="profit":
+	#		dict["fieldToSearch"] = a
+	
 	
 
+def connectToEdgar(chatInput):
+	chatInput = "What is the gross profit of AAPL"
+	dict = parsetext(chatInput)
+	company = dict["companyname"]
+	#resp = requests.get('http://edgaronline.api.mashery.com/v2/corefinancials/ann?primarysymbols=MSFT&appkey=5kb2erymmv7s5ne6ksqkxt2v')
+	resp = requests.get('http://edgaronline.api.mashery.com/v2/corefinancials/qtr?primarysymbols=' + company +  '&numperiods=1&appkey=5kb2erymmv7s5ne6ksqkxt2v')
+	#resp = requests.get('http://edgaronline.api.mashery.com/v2/companies?companynames=*micro*&limit=10&offset=10&sortby=companyName%20asc&appkey=5kb2erymmv7s5ne6ksqkxt2v')
+	#5kb2erymmv7s5ne6ksqkxt2v
+	#resp = requests.get('http://edgaronline.api.mashery.com/v2/insiders/summary?fields=issueid,insiderformtype,numTransactions,sumnumTransactions&filter=sumnumTransactions%20gt%201%20AND%20issueid%20eq%20467297&appkey=5kb2erymmv7s5ne6ksqkxt2v')
+	#if resp.status_code != 200:
+		# This means something went wrong.
+	#    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+	#for todo_item in resp.json():
+	#    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+
+	#print('Created task. ID: {}'.format(resp.json()["grossprofit"]))
+	 #
+
+	DictConv = {'profit': 'grossprofit'}
+
+	 
+	fq = ""
+	d1 = ""
+	d2 = ""
+	if "financialquarter" in dict:
+		fq = dict["financialquarter"]
+	fieldToSearch = dict["fieldToSearch"] #"grossprofit"
+	#debt ratio
+	#growth rate
+	#print((resp.json()["result"]["rows"][0]["values"][24]))
+	rows = (resp.json()["result"]["rows"][0]["values"])
+	for n in fieldToSearch:
+        #if n == "debt"
+		for row in rows:
+			if (n in DictConv) and (row["field"] == DictConv[n]):
+				print(row["value"])
+				return (str(row["value"]))
+
+
+
+	#url = 'http://edgaronline.api.mashery.com/v2/corefinancials/ann?primarysymbols=MSFT&appkey=5kb2erymmv7s5ne6ksqkxt2v'
+	#data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+	#response = requests.get(url, data=data)
+
+
+	#debt = debt ratio / liability ratio
+	#(totalLongterm + Short)present - (totalLongterm + Short)past / past = debt ratio
+	#(totalliabilities) present - (totalliabilities)present 
+
+
+
+
+
+	
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -44,7 +186,11 @@ def test_message(message):
 def test_broadcast_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
+         {'data': "Me:                     " + message['data'], 'count': session['receive_count']},
+         broadcast=True)
+    response = connectToEdgar(message)
+    emit('my_response',
+         {'data': "Crystal Ball: " + str(response), 'count': session['receive_count']},
          broadcast=True)
 
 
