a0c90fb5f919939680810781e4a5d12e2acd0ce0
Edgar Ruiz <ed.ruiz1106@gmail.com>
2016-10-21 05:01:31 -0400

    parse

--- slytherin/EdgarAccess.py
+++ slytherin/EdgarAccess.py
@@ -1,8 +1,18 @@
 import requests
+import urllib.parse
 
+encodedURL = 'http://edgaronline.api.mashery.com/v2/'
+query = 'insiders/summary?fields=issueid,insiderformtype,numTransactions,sumnumTransactions&filter=sumnumTransactions%20gt%201%20AND%20issueid eq 467297&appkey=5kb2erymmv7s5ne6ksqkxt2v'
+apiKey= '&appkey=5kb2erymmv7s5ne6ksqkxt2v'
+
+query = urllib.parse.urlencode(query)
+
+encodedURL = encodedURL +query+apiKey
 
 chatInput = ""
-resp = requests.get('http://edgaronline.api.mashery.com/v2/corefinancials/ann?primarysymbols=MSFT&appkey=5kb2erymmv7s5ne6ksqkxt2v')
+
+print (encodedURL)
+resp = requests.get(encodedURL)
 #5kb2erymmv7s5ne6ksqkxt2v
 #resp = requests.get('http://edgaronline.api.mashery.com/v2/insiders/summary?fields=issueid,insiderformtype,numTransactions,sumnumTransactions&filter=sumnumTransactions%20gt%201%20AND%20issueid eq 467297&appkey=5kb2erymmv7s5ne6ksqkxt2v')
 #if resp.status_code != 200:
@@ -11,4 +21,8 @@ resp = requests.get('http://edgaronline.api.mashery.com/v2/corefinancials/ann?pr
 #for todo_item in resp.json():
 #    print('{} {}'.format(todo_item['id'], todo_item['summary']))
 
-print('Created task. ID: {}'.format(resp.json()))#["amended"])
+print (resp.json())
+# what is the gross rate for compny x for year x
+
+#["result"]["rows"]["values"][27]
+#presentvalue pastvalue
--- slytherin/parse.py
+++ slytherin/parse.py
@@ -27,7 +27,7 @@ dict = {}
 # Get all the English alphabet letters and a space
 valid_letters = string.ascii_letters  + string.digits + ' '
 
-pattern = r'(\d+/\d+/\d+)'
+pattern = '[1-3][0-9]{3}'
 
 #match = re.findall(r'(\d+/\d+/\d+)',text)
 
@@ -52,7 +52,7 @@ def parsetext(text):
 	stripped_text = ''.join([char if char in valid_letters else '' for char in text])
 	tokens = word_tokenize(stripped_text)
 	tags = pos_tag(tokens)
-	match = re.findall(r'(\d+/\d+/\d+)',text)
+	match = re.findall(pattern,text)
 
 	for (a,b) in tags:
 		if b == 'NN' or b == 'NNP':
@@ -61,10 +61,21 @@ def parsetext(text):
 			dict["companyname"] = a
 		if a == 'first' or a == 'second' or a == 'third' or a == 'fourth':
 			dict["financialquarter"] = quarters[a]
+		if a =='debt':
+			dict["fieldToSearch"] = a
+		elif a =='growth':
+			dict["fieldToSearch"] = a
+		elif a =="profit":
+			dict["fieldToSearch"] = a
 
 	if match:		
 		for a in match:
-			dict["date"] = a
+			if "date" in dict:
+				dict["date2"] = a
+			else:
+				dict["date"] = a 
+				
+	
 			
 	return dict
 	
